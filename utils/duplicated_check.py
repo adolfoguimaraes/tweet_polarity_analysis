9751970aaebccc680fca0a0e3603b55006da28fe
Johnny William <50759662+Jownao@users.noreply.github.com>
2023-06-19 11:05:19 -0300

    Update and rename teste1.py to duplicated_check.py

--- utils/duplicated_check.py
+++ utils/duplicated_check.py
@@ -1,3 +1,5 @@
+# Verifica se existe arquivos duplicados e informa quais são
+
 import os
 
 def check_duplicate_files(folders):
@@ -17,11 +19,9 @@ def check_duplicate_files(folders):
 
 # Exemplo de uso
 folder_paths = [
-    r'E:\John\\aUNIT\TCC\collecting_tweets\coleta\\random4000\input',
-    r'E:\John\\aUNIT\TCC\collecting_tweets\coleta\\random4000\input1',
-    r'E:\John\\aUNIT\TCC\collecting_tweets\coleta\\random4000\input2',
-    r'E:\John\\aUNIT\TCC\collecting_tweets\coleta\\random4000\input3',
-    r'E:\John\\aUNIT\TCC\collecting_tweets\coleta\\random4000\input4',
+    r'C:\caminho\para\pasta',
+    r'C:\caminho\para\pasta',
+    r'C:\caminho\para\pasta',
     # Adicione quantas pastas desejar
 ]
 
